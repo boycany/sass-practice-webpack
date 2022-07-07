80394046cd5641c890e9c7794214d932f124ebcc
111084 <111084@w.tmu.edu.tw>
2022-07-07 11:45:53 +0800

    ch9-3 badge

--- src/index.js
+++ src/index.js
@@ -3,48 +3,48 @@ import "bootstrap/js/dist/collapse"; //引用 手風琴 摺疊效果
 // import "bootstrap/js/dist/alert";
 import Alert from "bootstrap/js/dist/alert"; //引用 alert 效果的 close功能
 
-function alertHandler() {
-  //custom alert
-  const alertCustomDiv = document.getElementById("alertCustom");
-  document.getElementById("alertCustomBtn").addEventListener("click", () => {
-    const alertCustom = new Alert(alertCustomDiv); //用這樣的方式僅引入 Alert 的部分，效能較好
-    alertCustom.close(); // close() 來自 bootstrap.Alert
-  });
+// function alertHandler() {
+//   //custom alert
+//   const alertCustomDiv = document.getElementById("alertCustom");
+//   document.getElementById("alertCustomBtn").addEventListener("click", () => {
+//     const alertCustom = new Alert(alertCustomDiv); //用這樣的方式僅引入 Alert 的部分，效能較好
+//     alertCustom.close(); // close() 來自 bootstrap.Alert
+//   });
 
-  //當關閉 alert 之後還想要有其他操作的話
-  alertCustomDiv.addEventListener("closed.bs.alert", function () {
-    console.log("closed....");
-  });
+//   //當關閉 alert 之後還想要有其他操作的話
+//   alertCustomDiv.addEventListener("closed.bs.alert", function () {
+//     console.log("closed....");
+//   });
 
-  //
-  const alertPlaceholder = document.getElementById("liveAlertPlaceholder");
-  const alertTrigger = document.getElementById("liveAlertBtn");
+//   //
+//   const alertPlaceholder = document.getElementById("liveAlertPlaceholder");
+//   const alertTrigger = document.getElementById("liveAlertBtn");
 
-  function alert(message, type) {
-    const wrapper = document.createElement("div");
-    wrapper.innerHTML =
-      '<div class="alert alert-' +
-      type +
-      ' alert-dismissible" role="alert">' +
-      message +
-      ' <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>';
+//   function alert(message, type) {
+//     const wrapper = document.createElement("div");
+//     wrapper.innerHTML =
+//       '<div class="alert alert-' +
+//       type +
+//       ' alert-dismissible" role="alert">' +
+//       message +
+//       ' <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>';
 
-    alertPlaceholder.append(wrapper);
-  }
+//     alertPlaceholder.append(wrapper);
+//   }
 
-  if (alertTrigger) {
-    alertTrigger.addEventListener("click", function () {
-      alert(
-        `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-activity me-2" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M6 2a.5.5 0 0 1 .47.33L10 12.036l1.53-4.208A.5.5 0 0 1 12 7.5h3.5a.5.5 0 0 1 0 1h-3.15l-1.88 5.17a.5.5 0 0 1-.94 0L6 3.964 4.47 8.171A.5.5 0 0 1 4 8.5H.5a.5.5 0 0 1 0-1h3.15l1.88-5.17A.5.5 0 0 1 6 2Z"/>
-</svg>Nice, you trigged this <a href="#" class="alert-link"> alert message!</a>`,
-        "danger"
-      );
-    });
-  }
-}
+//   if (alertTrigger) {
+//     alertTrigger.addEventListener("click", function () {
+//       alert(
+//         `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-activity me-2" viewBox="0 0 16 16">
+//   <path fill-rule="evenodd" d="M6 2a.5.5 0 0 1 .47.33L10 12.036l1.53-4.208A.5.5 0 0 1 12 7.5h3.5a.5.5 0 0 1 0 1h-3.15l-1.88 5.17a.5.5 0 0 1-.94 0L6 3.964 4.47 8.171A.5.5 0 0 1 4 8.5H.5a.5.5 0 0 1 0-1h3.15l1.88-5.17A.5.5 0 0 1 6 2Z"/>
+// </svg>Nice, you trigged this <a href="#" class="alert-link"> alert message!</a>`,
+//         "danger"
+//       );
+//     });
+//   }
+// }
 
-alertHandler();
+// alertHandler();
 
 // var alertList = document.querySelectorAll(".alert");
 // var alerts = [].slice.call(alertList).map(function (element) {
