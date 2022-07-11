8c7f78c8429d42b57d0aad233566ce9ec70df13b
111084 <111084@w.tmu.edu.tw>
2022-07-11 11:50:35 +0800

    ch9-6 carousel

--- src/index.js
+++ src/index.js
@@ -1,13 +1,43 @@
 import "./index.scss";
-import "bootstrap/js/dist/collapse"; //引用 手風琴 摺疊效果
+// import "bootstrap/js/dist/carousel";
 import Button from "bootstrap/js/dist/button";
+import Carousel from "bootstrap/js/dist/carousel";
 
-function buttonHandler() {
-  const button = document.getElementById("myButton");
-  const bsButton = new Button(button);
-  setTimeout(() => {
-    bsButton.toggle();
-  }, 2000);
+// function buttonHandler() {
+//   const button = document.getElementById("myButton");
+//   const bsButton = new Button(button);
+//   setTimeout(() => {
+//     bsButton.toggle();
+//   }, 2000);
+// }
+
+// buttonHandler();
+
+function carouselHandler() {
+  const myCarousel = document.getElementById("carouselExampleJS");
+  const myCarouselBtnPrev = document.getElementById("carouselExampleJSPrev");
+  const myCarouselBtnNext = document.getElementById("carouselExampleJSNext");
+
+  //相當於在 html 下一個 data-bs-ride 屬性
+  const carousel = new Carousel(myCarousel, {
+    interval: false,
+    touch: true,
+  });
+
+  myCarouselBtnPrev.addEventListener("click", function () {
+    carousel.prev();
+  });
+
+  myCarouselBtnNext.addEventListener("click", function () {
+    carousel.next();
+  });
+
+  myCarousel.addEventListener("slide.bs.carousel", function () {
+    console.log("slide....");
+  });
+
+  myCarousel.addEventListener("slid.bs.carousel", function () {
+    console.log("slid");
+  });
 }
-
-buttonHandler();
+carouselHandler();
