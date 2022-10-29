115d47e2f88c4fe44249005a38ee26a4b437cd66
boycany <yurayurateiko@gmail.com>
2022-10-29 20:20:01 +0800

    ch9-8 Dropdown done

--- src/index.js
+++ src/index.js
@@ -1,64 +1,16 @@
 import "./index.scss";
-// import "bootstrap/js/dist/carousel";
-import Button from "bootstrap/js/dist/button";
-import Carousel from "bootstrap/js/dist/carousel";
-import { Collapse } from "bootstrap";
+import Dropdown from "bootstrap/js/dist/dropdown";
 
-// function buttonHandler() {
-//   const button = document.getElementById("myButton");
-//   const bsButton = new Button(button);
-//   setTimeout(() => {
-//     bsButton.toggle();
-//   }, 2000);
-// }
-
-// buttonHandler();
-
-function carouselHandler() {
-    const myCarousel = document.getElementById("carouselExampleJS");
-    const myCarouselBtnPrev = document.getElementById("carouselExampleJSPrev");
-    const myCarouselBtnNext = document.getElementById("carouselExampleJSNext");
-
-    //相當於在 html 下一個 data-bs-ride 屬性
-    const carousel = new Carousel(myCarousel, {
-        interval: false,
-        touch: true,
+//須 npm i -D @popperjs/core
+function renderDropdown() {
+    const myDropdownBtn = document.getElementById("dropdownBtn");
+    const bsDropdown = new Dropdown(myDropdownBtn, {
+        autoClose: true,
     });
 
-    myCarouselBtnPrev.addEventListener("click", function () {
-        carousel.prev();
-    });
-
-    myCarouselBtnNext.addEventListener("click", function () {
-        carousel.next();
-    });
-
-    myCarousel.addEventListener("slide.bs.carousel", function () {
-        console.log("slide....");
-    });
-
-    myCarousel.addEventListener("slid.bs.carousel", function () {
-        console.log("slid");
-    });
-}
-carouselHandler();
-
-function renderCollapse() {
-    // var myCollapse = document.getElementById("myCollapse");
-    // var bsCollapse = new bootstrap.Collapse(myCollapse, {
-    //     toggle: false,
-    // });
-    const myCollapseBtn = document.getElementById("collapseBtnExampleJS");
-    console.log("myCollapseBtn :>> ", myCollapseBtn);
-
-    const myCollapse = document.getElementById("collapseExampleJS");
-    const bsCollapse = new Collapse(myCollapse, {
-        toggle: false,
-    });
-
-    myCollapseBtn.addEventListener("click", function () {
-        bsCollapse.toggle();
+    myDropdownBtn.addEventListener("click", function () {
+        bsDropdown.toggle();
     });
 }
 
-renderCollapse();
+renderDropdown();
