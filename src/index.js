c2fe852f2bb1fa4fdfadc75cf2c23df837ec116e
boycany <yurayurateiko@gmail.com>
2022-10-29 19:17:47 +0800

    ch9-7 done

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import "./index.scss";
 // import "bootstrap/js/dist/carousel";
 import Button from "bootstrap/js/dist/button";
 import Carousel from "bootstrap/js/dist/carousel";
+import { Collapse } from "bootstrap";
 
 // function buttonHandler() {
 //   const button = document.getElementById("myButton");
@@ -14,30 +15,50 @@ import Carousel from "bootstrap/js/dist/carousel";
 // buttonHandler();
 
 function carouselHandler() {
-  const myCarousel = document.getElementById("carouselExampleJS");
-  const myCarouselBtnPrev = document.getElementById("carouselExampleJSPrev");
-  const myCarouselBtnNext = document.getElementById("carouselExampleJSNext");
+    const myCarousel = document.getElementById("carouselExampleJS");
+    const myCarouselBtnPrev = document.getElementById("carouselExampleJSPrev");
+    const myCarouselBtnNext = document.getElementById("carouselExampleJSNext");
 
-  //相當於在 html 下一個 data-bs-ride 屬性
-  const carousel = new Carousel(myCarousel, {
-    interval: false,
-    touch: true,
-  });
+    //相當於在 html 下一個 data-bs-ride 屬性
+    const carousel = new Carousel(myCarousel, {
+        interval: false,
+        touch: true,
+    });
 
-  myCarouselBtnPrev.addEventListener("click", function () {
-    carousel.prev();
-  });
+    myCarouselBtnPrev.addEventListener("click", function () {
+        carousel.prev();
+    });
 
-  myCarouselBtnNext.addEventListener("click", function () {
-    carousel.next();
-  });
+    myCarouselBtnNext.addEventListener("click", function () {
+        carousel.next();
+    });
 
-  myCarousel.addEventListener("slide.bs.carousel", function () {
-    console.log("slide....");
-  });
+    myCarousel.addEventListener("slide.bs.carousel", function () {
+        console.log("slide....");
+    });
 
-  myCarousel.addEventListener("slid.bs.carousel", function () {
-    console.log("slid");
-  });
+    myCarousel.addEventListener("slid.bs.carousel", function () {
+        console.log("slid");
+    });
 }
 carouselHandler();
+
+function renderCollapse() {
+    // var myCollapse = document.getElementById("myCollapse");
+    // var bsCollapse = new bootstrap.Collapse(myCollapse, {
+    //     toggle: false,
+    // });
+    const myCollapseBtn = document.getElementById("collapseBtnExampleJS");
+    console.log("myCollapseBtn :>> ", myCollapseBtn);
+
+    const myCollapse = document.getElementById("collapseExampleJS");
+    const bsCollapse = new Collapse(myCollapse, {
+        toggle: false,
+    });
+
+    myCollapseBtn.addEventListener("click", function () {
+        bsCollapse.toggle();
+    });
+}
+
+renderCollapse();
