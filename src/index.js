21d3f90a37f3cfbf2dabe30d97b6014298e7d849
boycany <yurayurateiko@gmail.com>
2022-10-30 15:35:12 +0800

    offcanvas done

--- src/index.js
+++ src/index.js
@@ -4,3 +4,22 @@ import "./index.scss";
 //因為有用到 Dropdown 所以也要引入
 import Collapse from "bootstrap/js/dist/collapse";
 import Dropdown from "bootstrap/js/dist/dropdown";
+import Offcanvas from "bootstrap/js/dist/offcanvas";
+
+function offcanvasHandler() {
+    const myOffcanvasBtn = document.getElementById("offcanvasExampleJSBtn");
+    const myOffcanvas = document.getElementById("offcanvasExampleJS");
+
+    const myBsOffcanvas = new Offcanvas(myOffcanvas, {
+        //可在此物件加入 options
+        backdrop: false,
+        keyboard: false,
+        scroll: true,
+    });
+
+    myOffcanvasBtn.addEventListener("click", function () {
+        myBsOffcanvas.toggle();
+    });
+}
+
+offcanvasHandler();
