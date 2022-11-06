fbf65f7c93bc933f9e0ded2600df3699bf5c0ba4
boycany <yurayurateiko@gmail.com>
2022-11-06 17:41:23 +0800

    done

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
 import "./index.scss";
-import { Dropdown } from "bootstrap";
+
+const forms = document.querySelectorAll(".needs-validation");
+
+// Loop over them and prevent submission
+Array.from(forms).forEach((form) => {
+    form.addEventListener(
+        "submit",
+        (event) => {
+            if (!form.checkValidity()) {
+                event.preventDefault();
+                event.stopPropagation();
+            }
+
+            form.classList.add("was-validated");
+        },
+        false
+    );
+});
